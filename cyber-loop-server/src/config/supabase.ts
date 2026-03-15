7ce5123ea3ec4ba0fc449fb8aed1d5789087dfa9
Ali Hussain <ali.hussaino71205o@gmail.com>
2026-03-15 11:47:17 +0500

    Fixed type in env variable

--- cyber-loop-server/src/config/supabase.ts
+++ cyber-loop-server/src/config/supabase.ts
@@ -1,10 +1,10 @@
 import { createClient, SupabaseClient } from '@supabase/supabase-js';
 
-const url = process.env.SUPUBASE_PROJECT_URL;
+const url = process.env.SUPABASE_PROJECT_URL;
 const anonKey = process.env.SUPABASE_ANON_KEY;
 
 if (!url || !anonKey) {
-  throw new Error('Missing SUPUBASE_PROJECT_URL or SUPABASE_ANON_KEY');
+  throw new Error('Missing SUPABASE_PROJECT_URL or SUPABASE_ANON_KEY');
 }
 
 export const supabase: SupabaseClient = createClient(url.trim(), anonKey.trim());
